6edf9ef3112d53f5b010100dcc2b3a06152b13ad
yukangrong <yukangrong@myhexin.com>
2017-08-23 18:24:26 +0800

    webpack配置文件提交

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,40 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require("webpack");
 const path = require("path");
 
+
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        a:"./src/index.js",
+        b:"./src/search.js",
+        resource:"./src/resource.js"
+    },
     output:{
         path: path.resolve(__dirname,'dist'),
-        filename:"bundle.js"
+        filename:"[name].js"
+    },
+    devServer: {
+        contentBase: './dist'
     },
     module:{
         rules:[
             {
                 test: /\.css$/,
-                use: ["style-loader","css-loader"]
+                use:["style-loader","css-loader"]
+                /*use:ExtractTextPlugin.extract({ // 资源文件的提取
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                })*/
             },
             {
-                test: /\.png$/,
+                test: /\.(otf|ttf)$/,
+                use: ["file-loader"]
+            },
+            {
+                test: /\.(png|jpg)$/,
                 use: ["file-loader"]
             },
             {
@@ -27,10 +45,35 @@ module.exports = {
     },
     plugins:[
         new CleanWebpackPlugin(['dist']),
-        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),// 对打包出来的js做压缩处理
+        new ExtractTextPlugin('[name].css'),
+        /*new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.optimize\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: { discardComments: {removeAll: true } },
+            canPrint: true
+        }),*/
         new HtmlWebpackPlugin({
-            title:"HtmlWebpackPlugin",
-            filename:"abc.html"
-        })
+            template:"src/index.html",
+            filename:"abc.html",
+            inject:"head",
+            chunks:["a"]
+        }),
+        new HtmlWebpackPlugin({
+            template:"src/search.html",
+            filename:"search.html",
+            inject:"body",
+            chunks:["b"]
+        }),
+        new HtmlWebpackPlugin({
+            template:"src/resource.html",
+            filename:"resource.html",
+            inject:"body",
+            chunks:["resource"],
+            minify:{
+                minifyCSS:true
+            }
+        }),
+
     ]
 };
